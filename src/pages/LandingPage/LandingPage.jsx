e0a7751981c79d1794bc5230f29ee8427538f529
hicass <cassidyjwalters@gmail.com>
2023-08-21 12:13:16 -0700

    Create and style the top section of the landing page.

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,21 @@
+import './LandingPage.css';
+
 export default function LandingPage() {
     return (
-        <h1>LandingPage</h1>
+        <div className='landing-top'>
+            <h2>- Since 1933 -</h2>
+            <h1>Whiskered Grape Co.</h1>
+            <h3>crafting tradition, embracing innovation</h3>
+
+            <div id="scroll-wrapper">
+                <div id="scroll-wrapper-inner">
+                    <div id="scroll-title">
+                        Scroll
+                    </div>
+                    <div id="scroll-down">
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
