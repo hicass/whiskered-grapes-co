6ada9f30dfce22ef3edbf344e1e6321b5f53abeb
hicass <cassidyjwalters@gmail.com>
2023-10-18 15:53:06 -0700

    Polish the way the wine index page looks.

--- src/components/WinePage/WineItem/WineItem.jsx
+++ src/components/WinePage/WineItem/WineItem.jsx
@@ -1,20 +1,23 @@
 import { Link } from 'react-router-dom';
-import { Fade } from 'react-awesome-reveal';
 import './WineItem.css';
 
 export default function WineItem({ wine }) {
   return (
-    <>
-      <Fade fraction="0.8">
-        <h3 className="wine-item-name">{wine.name}</h3>
-      </Fade>
+    <div className="inner-wine-item-content">
+      <h3 className="wine-item-name">{wine.name}</h3>
       <img className="wine-item-img" src={wine.image} alt="Wine" />
-      <Fade fraction="0.8">
+
+      <div className="wine-item-bot">
         <p className="wine-item-description">{wine.taste}</p>
-      </Fade>
-      <Link className="wine-item-link" to={`/wine/${wine._id}`} state={{ wineId: wine._id }}>
-        Learn More
-      </Link>
-    </>
+
+        <Link
+          className="wine-item-link"
+          to={`/wine/${wine._id}`}
+          state={{ wineId: wine._id }}
+        >
+          Learn More
+        </Link>
+      </div>
+    </div>
   );
 }
--- src/pages/WinePage/WinePage.jsx
+++ src/pages/WinePage/WinePage.jsx
@@ -1,39 +1,40 @@
 import { useState, useEffect } from 'react';
+import { Fade } from 'react-awesome-reveal';
 import * as winesAPI from '../../utilities/wines-api';
 import Logo from '../../components/Logo/Logo';
 import WineItem from '../../components/WinePage/WineItem/WineItem';
 import './WinePage.css';
 
 export default function WinePage() {
-    const [wineList, setWineList] = useState([]);
-    const wineListItems = wineList.map(wine => 
-        <div className="wine-item-container">
-            <WineItem key={wine._id} wine={wine} />
+  const [wineList, setWineList] = useState([]);
+  const wineListItems = wineList.map((wine) => (
+    <div className="wine-item-container">
+      <Fade direction="up" triggerOnce="true">
+        <WineItem key={wine._id} wine={wine} />
+      </Fade>
+    </div>
+  ));
+
+  useEffect(function () {
+    async function getWines() {
+      const wines = await winesAPI.getAll();
+      setWineList(wines);
+    }
+    getWines();
+  }, []);
+
+  return (
+    <section id="wine-section">
+      <div id="wine-section-top">
+        <div id="wine-section-top-container">
+          <div id="wine-section-logo">
+            <Logo />
+          </div>
+          <h1 id="wine-section-h1">Wine</h1>
         </div>
-    );
+      </div>
 
-    useEffect(function() {
-        async function getWines() {
-            const wines = await winesAPI.getAll();
-            setWineList(wines);
-        }
-        getWines()
-    }, []);
-
-    return (
-        <section id="wine-section">
-            <div id="wine-section-top">
-                <div id="wine-section-top-container">
-                    <div id="wine-section-logo" >
-                        <Logo />
-                    </div>
-                    <h1 id="wine-section-h1">Wine</h1>
-                </div>
-            </div>
-
-            <div id="wine-list-container">
-                {wineListItems}
-            </div>
-        </section>
-    )
+      <div id="wine-list-container">{wineListItems}</div>
+    </section>
+  );
 }
