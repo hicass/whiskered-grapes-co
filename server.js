a14806a2abf7284be671d405bcada9323cdf95e6
hicass <cassidyjwalters@gmail.com>
2023-10-30 15:02:41 -0700

    Deploy to Heroku

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-// Always require and configure near the top
 require('dotenv').config();
-// Connect to the database
 require('./config/database');
 
 const app = express();
@@ -12,22 +10,15 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 
-// Configure both serve-favicon & static middleware
-// to serve from the production 'build' folder
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Middleware to check and verify a JWT and
-// assign the user object from the JWT to req.user
 app.use(require('./config/checkToken'));
 
 const port = process.env.PORT || 3001;
 
-// Put API routes here, before the "catch all" route
 app.use('/api/wines', require('./routes/api/wines'));
 
-// The following "catch all" route (note the *) is necessary
-// to return the index.html on all non-AJAX/API requests
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
